00a3f4e7d39d819dc725ed411c7e350e05ac1b66
91986 <91986@LAPTOP-AEECM1P8>
2024-07-20 09:54:04 +0530

    Add new user Test cases written by Sonali Automation Tester

---
+++ src/test/java/Tutorial1/Demo2.java
@@ -0,0 +1,10 @@
+package Tutorial1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Add new user Test cases written by Sonali Automation Tester");
+
+	}
+
+}
